715d51fc279a187cb00c1bbfb38b6a34bc4e3aab
Clayton Knapp <csjknapp@gmail.com>
2022-05-26 18:17:45 -0700

    delete route for Image

--- __tests__/images.test.js
+++ __tests__/images.test.js
@@ -4,27 +4,27 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('image route tests', () => {
-    beforeEach(() => {
-        return setup(pool);
-    });
+  beforeEach(() => {
+    return setup(pool);
+  });
     
-    afterAll(() => {
-        pool.end();
-    });
+  afterAll(() => {
+    pool.end();
+  });
 
   it('get all images associated with user ID', async () => {
     // const expected = [
     //   { imageName: 'avatar' },
     //   { imageName: '2nd image' }
     // ];  
-    const expected = ['avatar', '2nd image'];  
+    const expected = ['avatar', '2nd image'];
 
     const res = await request(app)
       //get images from user with id 1
       .get('/api/v1/images/1');
       
     expect(res.body).toEqual(expected);
-    });
+  });
 
   it('creates an image name entry', async () => {
     const expected = { imageName: 'fake', userId: '2' };
@@ -34,9 +34,21 @@ describe('image route tests', () => {
       .send({ imageName: 'fake', userId: '2' });
     
     expect(res.body).toEqual(expected);
-
    
-  })
+  });
+
+  it('deletes an image by name', async () => {
+    const expected = { imageName: '3rd image', userId: '2' };
+
+    const res = await request(app)
+      .delete('/api/v1/images/3rd image');
+    
+    expect(res.body).toEqual(expected);
+
+
+
+
+  });
 
 
 
